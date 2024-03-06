23dac5692a2cbd4400a4d3662299c5f8709a31d4
amir <usernameamir18@gmail.com>
2024-03-05 23:56:45 -0800

    from amir

--- app/src/main/java/com/nitrogen/hydro/LoginActivity.java
+++ app/src/main/java/com/nitrogen/hydro/LoginActivity.java
@@ -5,28 +5,95 @@ import androidx.appcompat.widget.AppCompatTextView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputEditText number;
+    CountDownTimer cdt;
+    int cdtTime = 3;
+
+    TextInputLayout number  , code , id , name , posti;
+    TextInputEditText etnumber  , etcode , etid , etname , etposti;
+    AppCompatTextView t1 , t2 , timer;
     AppCompatTextView submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        number = findViewById(R.id.et_number);
+
+        number = findViewById(R.id.number);
+        code = findViewById(R.id.code);
+        posti = findViewById(R.id.posti);
+        id = findViewById(R.id.id);
+        name = findViewById(R.id.name);
+
+
+        etnumber = findViewById(R.id.et_number);
+        name = findViewById(R.id.name);
+
+        etcode = findViewById(R.id.et_code);
+        etposti = findViewById(R.id.et_posti);
+        etid = findViewById(R.id.et_id);
+        etname = findViewById(R.id.et_name);
+
+        t1 = findViewById(R.id.text1);
+        t2 = findViewById(R.id.text2);
+
+
+        timer = findViewById(R.id.timer);
+
+
         submit = findViewById(R.id.submit);
+
+
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this , InfoActivity.class));
-                getSharedPreferences("account" , MODE_PRIVATE).edit().putString("phone" , number.getText().toString()).apply();
-                finish();
+
+                if(etnumber.getText().toString().equals("") || etnumber.getText().toString().length()<11 || !etnumber.getText().toString().startsWith("0")) {
+                   number.setError("شماره تلفن خود را به صورت صحیح وارد کنید!");
+
+                }else {
+                    code.setVisibility(View.VISIBLE);
+                    number.setVisibility(View.GONE);
+                    t1.setText("کد تایید");
+                    t2.setText("لطفا کد احراز هویت را وارد کنید!");
+                    submit.setText("بررسی کد");
+
+
+                    time();
+                /*startActivity(new Intent(LoginActivity.this , InfoActivity.class));*/
+
+            }
+
             }
         });
 
     }
+    public  void time(){
+
+        cdt = new CountDownTimer(cdtTime * 40000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timer.setText((millisUntilFinished / 1000) + " " +" ثانیه مانده");
+                timer.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onFinish() {
+
+                timer.setText("ارسال مجدد کد فعالسازی");
+            }
+
+        }.start();
+
+
+    }
+
+
 }
--- app/src/main/java/com/nitrogen/hydro/SplashActivity.java
+++ app/src/main/java/com/nitrogen/hydro/SplashActivity.java
@@ -2,28 +2,84 @@ package com.nitrogen.hydro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Dialog dDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        if (isConnectedToInternet()) {
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                finish();
-                if (getSharedPreferences("account" , MODE_PRIVATE).getBoolean("open" , false) == true){
-                    startActivity(new Intent(SplashActivity.this , MainActivity.class));
-                }else {
-                    startActivity(new Intent(SplashActivity.this , LoginActivity.class));
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    finish();
+                    if (getSharedPreferences("account" , MODE_PRIVATE).getBoolean("open" , false) == true){
+                        startActivity(new Intent(SplashActivity.this , MainActivity.class));
+                    }else {
+                        startActivity(new Intent(SplashActivity.this , LoginActivity.class));
+                    }
                 }
+            } , 2000);
+        }else {
+            showDisConnectApp();
+        }
+
+    }
+
+    public void showDisConnectApp() {
+        AlertDialog.Builder aAlert = new AlertDialog.Builder(SplashActivity.this);
+        View vAlert = LayoutInflater.from(SplashActivity.this).inflate(R.layout.errorwifi_dialog, null);
+        aAlert.setView(vAlert);
+        Button bYes = vAlert.findViewById(R.id.btnRetry);
+
+        aAlert.setCancelable(false);
+
+        bYes.setText("بستن");
+        bYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
-        } , 2000);
+        });
+
+
+
+        dDialog = aAlert.create();
+        dDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        dDialog.show();
+    }
+    boolean isConnectedToInternet() {
+
+        ConnectivityManager cm =
+                (ConnectivityManager)SplashActivity.this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+
+        return isConnected;
+
     }
 }
